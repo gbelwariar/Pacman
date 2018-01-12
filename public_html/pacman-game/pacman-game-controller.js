fb99bebc766991a249d8e3e7ae2559e421e35ca4
Rachit Belwariar <gbelwariar@gmail.com>
2018-01-12 14:45:39 +0530

    Add documentation to the public APIs in pacman-game-controller.

--- public_html/pacman-game/pacman-game-controller.js
+++ public_html/pacman-game/pacman-game-controller.js
@@ -2,10 +2,22 @@ function PacmanGameCtrl(scoreboardService) {
     this.scoreboardService = scoreboardService;
 };
 
+
+/**
+ * Eat the pac-dot and earn points.
+ * 
+ * @param {number} pointsToEarn The number of points to earn.
+ */				
 PacmanGameCtrl.prototype.eat = function(pointsToEarn) {
     this.scoreboardService.eat(pointsToEarn);
 };
 
+
+/**
+ * Fetches the current score from scoreboard.
+ * 
+ * @return {number}
+ */				
 PacmanGameCtrl.prototype.getScore = function() {
     return this.scoreboardService.score;
 };
